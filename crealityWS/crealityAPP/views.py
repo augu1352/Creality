b1ddbc5e655e66af322930ca404a635253bde325
August Emmery-Funch <augustemmeryfunch@gmail.com>
2018-10-10 00:02:24 +0200

    ahaaha

--- crealityWS/crealityAPP/views.py
+++ crealityWS/crealityAPP/views.py
@@ -3,5 +3,5 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return render(request, 'crealityAPP/index.html')
+    return render(request, 'index.html')
     # return HttpResponse("HELLO WORLD")
--- crealityWS/crealityWS/urls.py
+++ crealityWS/crealityWS/urls.py
@@ -19,7 +19,7 @@ from django.conf.urls import url, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    url(r'^$', include('crealityAPP.urls')),
+    path('', include('crealityAPP.urls')),
 ]
 
 
