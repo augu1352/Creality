c9c9f9998fbbb6d4e86f4f87ad9ea421a2244f46
August Emmery-Funch <augustemmeryfunch@gmail.com>
2018-10-09 23:44:37 +0200

    Update crealityWS/crealityAPP/views.py

--- crealityWS/crealityAPP/views.py
+++ crealityWS/crealityAPP/views.py
@@ -3,4 +3,5 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse("HELLO WORLD")
+    return render(request, './home/index.html')
+    # return HttpResponse("HELLO WORLD")
