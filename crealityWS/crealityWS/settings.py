8159ed5b78201858f92262948d6e07ae1856f260
August Emmery-Funch <augustemmeryfunch@gmail.com>
2018-10-09 20:44:59 +0200

    add index

--- crealityWS/crealityWS/settings.py
+++ crealityWS/crealityWS/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'crealityWS.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ["home/index.html"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -106,7 +106,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'UTC+2'
 
 USE_I18N = True
 
