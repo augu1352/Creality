6b0abe27b14fdd9f3b4e56d0bcd44186463cf28b
August Emmery-Funch <augustemmeryfunch@gmail.com>
2018-10-10 00:03:55 +0200

    Update crealityWS/crealityWS/settings.py

--- crealityWS/crealityWS/settings.py
+++ crealityWS/crealityWS/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = ["192.168.1.171"]
 # Application definition
 
 INSTALLED_APPS = [
+    'crealityAPP'
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
