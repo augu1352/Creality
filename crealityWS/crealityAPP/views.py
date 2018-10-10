fa344cd12ac33e9c1472312e1c9d30b4a9245547
August Emmery-Funch <augustemmeryfunch@gmail.com>
2018-10-10 13:22:20 +0200

    Update crealityWS/crealityAPP/views.py

--- crealityWS/crealityAPP/views.py
+++ crealityWS/crealityAPP/views.py
@@ -16,13 +16,13 @@ def get_newUserInfo(request):
     if request.method == "POST":
         form = CreatUserForm(request.POST)
         if form.is_valid():
-            username = form.cleaned_data['username']
-            email = form.cleaned_data['email']
-            password = form.cleaned_data['password']
-            recipients = ["august12.feb2004@gmail.com"]
-            send_email("Created User", f"You created user: {username} with email: {email} and password: {password}", email, recipients)
+            # username = form.cleaned_data['username']
+            # email = form.cleaned_data['email']
+            # password = form.cleaned_data['password']
+            # recipients = ["august12.feb2004@gmail.com"]
+            # send_email("Created User", f"You created user: {username} with email: {email} and password: {password}", email, recipients)
             return HttpResponseRedirect("/thanks/")
     else:
         form = CreatUserForm()
 
-    return render(request, 'index.html', {'form': form})
+    return render(request, 'index.html', {"form" : form,})
