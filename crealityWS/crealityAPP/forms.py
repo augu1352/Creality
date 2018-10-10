e870abdfc305a220b3f609cfed1fbd026721f9f1
August Emmery-Funch <augustemmeryfunch@gmail.com>
2018-10-10 09:19:47 +0200

    Update crealityWS/crealityAPP/forms.py

--- crealityWS/crealityAPP/forms.py
+++ crealityWS/crealityAPP/forms.py
@@ -2,6 +2,6 @@ from django import forms
 
 
 class CreatUserForm(forms.Form):
-    username = forms.CharField(label='username', max_lenth=50)
+    username = forms.CharField(label='username', max_length=50)
     email = forms.EmailField()
     password = forms.PasswordInput()
