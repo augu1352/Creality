a52954790fd6d3818b29acdf919d92e668b38634
August Emmery-Funch <augustemmeryfunch@gmail.com>
2018-10-30 20:19:52 +0100

    Update crealityWS/crealityWS/settings.py

--- crealityWS/crealityWS/settings.py
+++ crealityWS/crealityWS/settings.py
@@ -14,7 +14,7 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-DB_DIR = os.environ['crealityDB']
+DB_DIR = 'var/lib/postgresql/10/main'
 
 
 # Quick-start development settings - unsuitable for production
