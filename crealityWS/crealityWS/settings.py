c112734658a078f2a43d2c6c0db5156431754f43
August Emmery-Funch <augustemmeryfunch@gmail.com>
2018-10-30 19:50:02 +0100

    Update crealityWS/crealityWS/settings.py

--- crealityWS/crealityWS/settings.py
+++ crealityWS/crealityWS/settings.py
@@ -82,7 +82,7 @@ DATABASES = {
     # }
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'crealityDB',
+        'NAME': os.path.join('/var/lib/', 'crealityDB'),
         'user': 'augu1352',
         'PASSWORD': '120204Aj',
         'HOST': 'localhost',
