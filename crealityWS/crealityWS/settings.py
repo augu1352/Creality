a258bb77a248575a2880d96320fe9b4a23de673b
August Emmery-Funch <augustemmeryfunch@gmail.com>
2018-10-30 20:23:05 +0100

    Update crealityWS/crealityWS/settings.py

--- crealityWS/crealityWS/settings.py
+++ crealityWS/crealityWS/settings.py
@@ -14,7 +14,7 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-DB_DIR = 'var/lib/postgresql/10/main'
+DB_DIR = 'root/var/lib/postgresql/10/main'
 
 
 # Quick-start development settings - unsuitable for production
@@ -83,7 +83,7 @@ DATABASES = {
     # }
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': os.path.join(DB_DIR, 'db.crealityDB'),
+        'NAME': os.path.join(DB_DIR, 'crealityDB'),
         'user': 'augu1352',
         'PASSWORD': '120204Aj',
         'HOST': '',
