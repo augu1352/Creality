200578ec7f945f130316f6ecd0e1d32178985371
August Emmery-Funch <augustemmeryfunch@gmail.com>
2018-11-13 17:03:34 +0100

    Update crealityWS/crealityAPP/urls.py

--- crealityWS/crealityAPP/urls.py
+++ crealityWS/crealityAPP/urls.py
@@ -3,7 +3,6 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path("", views.get_newUserInfo, name="get_newUserInfo"),
     path("login/", views.login, name="login"),
     path("createuser/", views.createUser, name="createUser")
 ]
