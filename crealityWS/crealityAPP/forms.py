3005c6cb33a8b87d4350a54b281c83dc8df171e2
August Emmery-Funch <augustemmeryfunch@gmail.com>
2018-11-13 17:01:42 +0100

    delete forms; create user form

--- crealityWS/crealityAPP/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-
-
-class CreatUserForm(forms.Form):
-    username = forms.CharField(label='username', max_length=50)
-    email = forms.EmailField()
-    password = forms.PasswordInput()
