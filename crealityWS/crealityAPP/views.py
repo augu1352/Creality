06426785d375dffb710aa5c99e110d9d31e54867
August Emmery-Funch <augustemmeryfunch@gmail.com>
2018-11-13 17:02:49 +0100

    Update crealityWS/crealityAPP/views.py

--- crealityWS/crealityAPP/views.py
+++ crealityWS/crealityAPP/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect
-from .forms import CreatUserForm
 from django.core.mail import send_mail
 
 # Create your views here.
@@ -19,17 +18,17 @@ def createUser(request):
 
 #form username handler
 
-def get_newUserInfo(request):
-    if request.method == "POST":
-        form = CreatUserForm(request.POST)
-        if form.is_valid():
-            # username = form.cleaned_data['username']
-            # email = form.cleaned_data['email']
-            # password = form.cleaned_data['password']
-            # recipients = ["august12.feb2004@gmail.com"]
-            # send_email("Created User", f"You created user: {username} with email: {email} and password: {password}", email, recipients)
-            return HttpResponseRedirect("/thanks/")
-    else:
-        form = CreatUserForm()
-
-    return render(request, 'index.html', {'formHere' : form,})
+# def get_newUserInfo(request):
+#     if request.method == "POST":
+#         form = CreatUserForm(request.POST)
+#         if form.is_valid():
+#             # username = form.cleaned_data['username']
+#             # email = form.cleaned_data['email']
+#             # password = form.cleaned_data['password']
+#             # recipients = ["august12.feb2004@gmail.com"]
+#             # send_email("Created User", f"You created user: {username} with email: {email} and password: {password}", email, recipients)
+#             return HttpResponseRedirect("/thanks/")
+#     else:
+#         form = CreatUserForm()
+#
+#     return render(request, 'index.html', {'formHere' : form,})
