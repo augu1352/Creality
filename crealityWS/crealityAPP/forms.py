d108bf5b4a5f40bece0f0b1463be2f5bcb6420a9
August Emmery-Funch <augustemmeryfunch@gmail.com>
2018-11-13 19:47:06 +0100

    Update crealityWS/crealityAPP/forms.py

--- crealityWS/crealityAPP/forms.py
+++ crealityWS/crealityAPP/forms.py
@@ -2,6 +2,6 @@ from django import forms
 
 
 class CreateUserForm(forms.Form):
-    username = forms.CharField(label="Desired Username")
-    email = forms.EmailField(label="Valid Email Address")
+    username = forms.CharField(label="Username")
+    email = forms.EmailField(label="Email Address")
     password = forms.CharField(widget=forms.PasswordInput())
