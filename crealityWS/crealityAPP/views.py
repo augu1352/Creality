5a363cf828f399eb51e496f8ad21f70eac864c13
August Emmery-Funch <augustemmeryfunch@gmail.com>
2019-01-22 17:41:27 +0100

    Update crealityWS/crealityAPP/views.py

--- crealityWS/crealityAPP/views.py
+++ crealityWS/crealityAPP/views.py
@@ -216,7 +216,7 @@ def viewImage(request):
             imgSize[imgSize.index(n)] = int(n)
         imgSize = tuple(imgSize)
         print(imgSize)
-        image = Image.frombytes(i[1], imgSize, i[0], "base64", i[1])
+        image = Image.frombytes(i[1], imgSize, i[0])
         images.append(image)
     print(images)
 
